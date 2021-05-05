2901fd6384a81ecf120b8ad57de104ee0d2ddbce
Devendra <yadavdevendra99876@gmail.com>
2021-05-05 23:15:39 +0530

    changed some default values and meta values for models

--- pizzaproject/pizzaApp/models.py
+++ pizzaproject/pizzaApp/models.py
@@ -11,10 +11,15 @@ class pizzaDropdown(models.Model):
     is_delete=models.IntegerField(db_column='is_delete',default=0)# 0 for not deleted 1 for deleted
     is_edit= models.IntegerField(db_column='is_edit',default=0)#same as above
     #using this model we can crete as much as parameter and values for creating & editing pizza
+    class Meta:
+        db_table='Pizzadropdown'
+        managed=True
 
 class pizzaCreated(models.Model):
     type=models.ForeignKey(pizzaDropdown,related_name='pizzatype',on_delete=models.SET_NULL,null=True)
     size= models.ForeignKey(pizzaDropdown,related_name='pizzasize',on_delete=models.SET_NULL,null=True)
     toppings= models.ForeignKey(pizzaDropdown,related_name='pizzatoppings',on_delete=models.SET_NULL,null=True)
-    is_edit= models.IntegerField(db_column='is_edit')# 0 for not deleted 1 for deleted
-
+    is_edit= models.IntegerField(db_column='is_edit',default=0)# 0 for not deleted 1 for deleted
+    class Meta:
+        db_table='Pizzacreated'
+        managed=True

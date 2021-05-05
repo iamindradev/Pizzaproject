14b59d4834fc967120686ae7eece04ce00aaac7c
Devendra <yadavdevendra99876@gmail.com>
2021-05-05 23:16:16 +0530

    added all the urls fro create ,edit, and list of created pizza

--- pizzaproject/pizzaApp/urls.py
+++ pizzaproject/pizzaApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import createPizzaparameter,getParameter
+from .views import createPizzaparameter,getParameter,createPizza,pizzaAlreadyCreated,pizzaEdit#here we also can use * but it creates ambiguity when we use more apps and views
 
 urlpatterns = [
     path('CreatePizzaparameter/',createPizzaparameter),#for creating pizza 
-    path('getParameter/',getParameter)#for getting the predefined parameters
+    path('getParameter/',getParameter),#for getting the predefined parameters
+    path('createPizza/',createPizza),#for creating pizza of your wish and(size,type,toppings)
+    path('pizzaAlreadycreated/',pizzaAlreadyCreated),#for getting the list of combination of pzza already created
+    path('pizzaEdit/',pizzaEdit)# for editng pizza alredy created
 
 ]

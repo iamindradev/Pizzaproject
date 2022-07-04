c4afb6aa5aa4eae363d2bdde95c88cec7ca04bb7
Devendra <yadavdevendra99876@gmail.com>
2022-07-04 16:29:18 +0530

    git changed requirements and settings

--- pizzaproject/pizzaproject/settings.py
+++ pizzaproject/pizzaproject/settings.py
@@ -74,18 +74,24 @@ WSGI_APPLICATION = 'pizzaproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':'pizzaproject',
-      'USER':'devendra',
-      'PASSWORD':'********',
-######## HERE WE SHOULD ALSO PUT OUR SETTINGS TO .gitignore FOR OUR PRIVATE PROJECT ###
-      'HOST':'localhost',
-      'PORT':'5432',
-   }
-}
+# DATABASES={
+#    'default':{
+#       'ENGINE':'django.db.backends.postgresql_psycopg2',
+#       'NAME':'pizzaproject',
+#       'USER':'devendra',
+#       'PASSWORD':'********',
+# ######## HERE WE SHOULD ALSO PUT OUR SETTINGS TO .gitignore FOR OUR PRIVATE PROJECT ###
+#       'HOST':'localhost',
+#       'PORT':'5432',
+#    }
+# }
 
+DATABASES={
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

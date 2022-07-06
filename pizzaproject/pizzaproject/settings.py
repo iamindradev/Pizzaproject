338471e43d515cab3aa63770846d5cfadf2acf1b
Devendra <yadavdevendra99876@gmail.com>
2022-07-06 16:50:10 +0530

    hosts changed

--- pizzaproject/pizzaproject/settings.py
+++ pizzaproject/pizzaproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z^o==xa=m*(o_lk1c@!_y*&02#83_2^f@8b*b2n-kkwxnr6_qw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
